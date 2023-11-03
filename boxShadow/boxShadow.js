f62bb090aa807cc864637561c869a1957f1ed8c2
Aureltus <aureltusr@gmail.com>
2023-11-03 12:00:20 +0100

    mise en forme

--- boxShadow/boxShadow.js
+++ boxShadow/boxShadow.js
@@ -1,3 +1,7 @@
+/****************************************
+ * Initialisation et configuration
+ ****************************************/
+
 const box = document.querySelector(".box"),
   blur = document.getElementById("blur"),
   color = document.getElementById("colorShadow"),
@@ -15,54 +19,42 @@ const widthBox = document.getElementById("widthBox"),
   colorBox = document.getElementById("colorShadowBox");
 
 const shadowPreview = document.getElementById("shadowPreview");
+// tableau contenant les ombres
 const previousShadows = [];
 
 box.style.boxShadow = `${axeX.value}px ${axeY.value}px ${blur.value}px ${spread.value}px ${color.value}`;
 
-color.addEventListener("input", updateShadowPreview);
-insetCheckbox.addEventListener("input", updateShadowPreview);
+// Déclare une variable pour combiner plusieurs ombres
 let combinedShadows;
 
-function updateShadowPreview() {
-  let opacityAdjusted =
-    opacity.value == 1 ? "" : Math.floor(opacity.value * 255).toString(16);
-  if (opacityAdjusted.length == 1) {
-    opacityAdjusted = 0 + opacityAdjusted;
-  }
-  /*const*/ shadowStyle = `${insetCheckbox.checked ? "inset" : ""} ${
-    axeX.value
-  }px ${axeY.value}px ${blur.value}px ${spread.value}px ${
-    color.value
-  }${opacityAdjusted}`;
-  previousShadows.pop(); // Supprimer la dernière ombre pour éviter les doublons
-  previousShadows.push(shadowStyle);
-  /*const*/ combinedShadows = previousShadows.join(", ");
-  shadowPreview.style.boxShadow = combinedShadows;
+const previousShadowsContainer = document.getElementById(
+  "previousShadowsContainer"
+);
 
-  copyInput();
-}
+updatePreviousShadows();
+
+/****************************************
+ * Gestion des événements
+ ****************************************/
+
+color.addEventListener("input", updateShadowPreview);
+insetCheckbox.addEventListener("input", updateShadowPreview);
 
 btn.addEventListener("click", () => {
+  // Ajoute l'ombre générée à la liste des ombres précédentes
   previousShadows.push(shadowPreview.style.boxShadow); // Ajouter l'ombre de l'aperçu à la liste des ombres précédentes
   const combinedShadows = previousShadows.join(", ");
   box.style.boxShadow = shadowPreview.style.boxShadow; // Appliquer l'ombre à la boîte
   updatePreviousShadows();
-  updateShadowPreview();
 });
 
-function copyInput() {
-  const elem = document.querySelector(".shadowInput");
-  elem.value = combinedShadows;
-}
-
-function boxSize() {
-  box.style.height = `${heightBox.value}px`;
-  box.style.width = `${widthBox.value}px`;
-  box.style.borderRadius = `${borderRadius.value}px`;
-  box.style.backgroundColor = `${colorBox.value}`;
-}
-
 colorBox.addEventListener("input", boxSize);
+
+const btnCopie = document.getElementById("copie");
+btnCopie.addEventListener("click", () => {
+  document.getElementById("shadowInput").select();
+  document.execCommand("copy");
+});
 
 const rangeResults = document.querySelectorAll(".range-result");
 rangeResults.forEach(function (rangeResult) {
@@ -83,9 +75,11 @@ numResult.forEach(function (numResult) {
   if (rangeInput) {
     numResult.value = rangeInput.value;
     rangeInput.addEventListener("change", function () {
+      // Vérifie si la valeur dépasse la limite supérieure
       if (Number(rangeInput.value) > Number(numResult.max)) {
         rangeInput.value = numResult.max;
       }
+      // Vérifie si la valeur est inférieure à la limite inférieure
       if (Number(rangeInput.value) < Number(numResult.min)) {
         rangeInput.value = numResult.min;
       }
@@ -95,20 +89,60 @@ numResult.forEach(function (numResult) {
     });
   }
 });
-const previousShadowsContainer = document.getElementById(
-  "previousShadowsContainer"
-);
+
+/****************************************
+ * Fonctions
+ ****************************************/
+
+//Met à jour en temps réel l'ombre
+function updateShadowPreview() {
+  // Récupère l'opacité ajustée (sous forme hexadécimale)
+  let opacityAdjusted =
+    opacity.value === 1 ? "" : Math.floor(opacity.value * 255).toString(16);
+  if (opacityAdjusted.length === 1) {
+    // Ajoute un zéro de remplissage si nécessaire
+    opacityAdjusted = 0 + opacityAdjusted;
+  }
+  // Crée la chaîne d'ombre avec les valeurs actuelles
+  const shadowStyle = `${insetCheckbox.checked ? "inset" : ""} ${
+    axeX.value
+  }px ${axeY.value}px ${blur.value}px ${spread.value}px ${
+    color.value
+  }${opacityAdjusted}`;
+  previousShadows.pop(); // Supprimer la dernière ombre pour éviter les doublons
+  previousShadows.push(shadowStyle); // Ajoute l'ombre actuelle à la liste des ombres précédentes
+  combinedShadows = previousShadows.join(", "); // Combine toutes les ombres dans une seule chaîne
+  shadowPreview.style.boxShadow = combinedShadows;
+
+  copyInput(); // Copie la valeur générée dans l'input de copie
+}
+
+// Copie la valeur générée dans le presse-papiers
+function copyInput() {
+  const elem = document.querySelector(".shadowInput");
+  elem.value = combinedShadows;
+}
+
+// Met à jour la taille et la couleur de la box
+function boxSize() {
+  box.style.height = `${heightBox.value}px`;
+  box.style.width = `${widthBox.value}px`;
+  box.style.borderRadius = `${borderRadius.value}px`;
+  box.style.backgroundColor = `${colorBox.value}`;
+}
 
 function updatePreviousShadows() {
+  // Efface le contenu de l'élément HTML qui affiche les bouttons des ombres
   previousShadowsContainer.innerHTML = "";
-
+  // Parcourt la liste des ombres dans le tableau des ombres
   previousShadows.forEach((shadow, index) => {
     if (index !== previousShadows.length - 1) {
-      // Exclure la dernière ombre
+      // Exclure le dernier element (qui contient toutes les ombres )
 
+      // Crée un élément div pour le bouton de suppression de l'ombre
       const shadowElement = document.createElement("div");
       shadowElement.classList.add("shadow-item"); // Ajout de la classe CSS
-      shadowElement.textContent = `shadow${index + 1}`;
+      shadowElement.textContent = `shadow${index + 1}`; //ajout d'un nom via l'index
       // utiliser shadow pour avoir la valeur de l'ombre au survole
       shadowElement.title = shadow;
 
@@ -116,21 +150,17 @@ function updatePreviousShadows() {
       deleteButton.textContent = "Supprimer";
       deleteButton.classList.add("btn"); // Ajout de la classe CSS
 
+      // Crée un bouton de suppression pour supprimer l'ombre
       deleteButton.addEventListener("click", () => {
         previousShadows.splice(index, 1);
         updatePreviousShadows();
         updateShadowPreview();
       });
 
+      // Ajoute le bouton de suppression à l'ombre
       shadowElement.appendChild(deleteButton);
       previousShadowsContainer.appendChild(shadowElement);
     }
   });
 }
-updatePreviousShadows();
-
-const btnCopie = document.getElementById("copie");
-btnCopie.addEventListener("click", () => {
-  document.getElementById("shadowInput").select();
-  document.execCommand("copy");
-});
+/****************************************/
